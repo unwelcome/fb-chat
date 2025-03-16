5c6974bd7af2c20e37977a0e475ada6704baa0c8
unwelcome <moyapohtaxd@mail.ru>
2025-03-16 16:11:49 +0300

    welcome page

---
+++ frontend/Talker/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <RouterView />
+</template>
+<script lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+
+export default{
+
+};
+
+</script>
---
+++ frontend/Talker/src/pages/WelcomePage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class=" w-svw h-svh flex flex-col justify-center items-center">
+    <div class=" flex flex-col bg-color-1 rounded-xl p-8 justify-start items-stretch gap-5">
+      <h2 class="text-xl text-center">Welcome to Talker!</h2>
+      <p class="text-lg text-justify mb-5">To use Talker, please register </p>
+      <div class="flex flex-row gap-5 justify-center items-center">
+        <input type="button" class="btn btn-main bg-slate-400" value="Log In"/>
+        <p class="text-lg text-color-description">or</p>
+        <input type="button" class="btn btn-main" value="Sign In"/>
+      </div>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+export default{
+
+}
+</script>
